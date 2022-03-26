72609dd09da8f5fcd43524f29ab767ad5e408082
joshmcorreia <joshmcorreia@gmail.com>
2022-03-26 16:15:56 -0700

    Split up sections when reading in file

--- reverser.py
+++ reverser.py
@@ -13,13 +13,22 @@ def replace_text_with_emoji(input_text):
 	return input_text
 
 def read_input_file():
+	"""
+	Reads in the input file `input.txt` located in the same folder.
+	"""
+	wordle_entries = []
 	lines = []
 	with open("input.txt", 'r') as file_in:
 		for line in file_in:
 			stripped_line = line.strip()
 			line_with_emojis = replace_text_with_emoji(stripped_line)
 			lines.append(line_with_emojis)
-	return lines
+
+			contains_word = any(char.isalpha() for char in line_with_emojis)
+			if contains_word:
+				wordle_entries.append(lines)
+				lines = []
+	return wordle_entries
 
 def main():
 	lines = read_input_file()

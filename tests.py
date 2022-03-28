6e0645f80856f2891f8a892e1f5e3af75dbbfcbf
joshmcorreia <joshmcorreia@gmail.com>
2022-03-27 22:48:35 -0700

    Add unit tests for white emojis

--- tests.py
+++ tests.py
@@ -9,6 +9,12 @@ class BasicTest(unittest.TestCase):
 		expected_output = "⬛⬛⬛⬛🟨"
 		self.assertEqual(result, expected_output)
 
+	def test_replace_text_with_emoji_white(self):
+		input_string = ":white_large_square::white_large_square::white_large_square::white_large_square::large_yellow_square:"
+		result = replace_text_with_emoji(input_string)
+		expected_output = "⬛⬛⬛⬛🟨"
+		self.assertEqual(result, expected_output)
+
 	def test_replace_text_with_emoji_all_colors(self):
 		input_string = ":black_large_square::black_large_square::black_large_square::large_green_square::large_yellow_square:"
 		result = replace_text_with_emoji(input_string)
@@ -17,6 +23,12 @@ class BasicTest(unittest.TestCase):
 
 	def test_replace_text_with_emojis_already_emojis(self):
 		input_string = "⬛⬛⬛🟩🟨"
+		result = replace_text_with_emoji(input_string)
+		expected_output = "⬛⬛⬛🟩🟨"
+		self.assertEqual(result, expected_output)
+
+	def test_replace_text_with_emojis_already_emojis_white(self):
+		input_string = "⬜⬜⬜🟩🟨"
 		result = replace_text_with_emoji(input_string)
 		expected_output = "⬛⬛⬛🟩🟨"
 		self.assertEqual(result, expected_output)

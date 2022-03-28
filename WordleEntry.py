7afbcd5fe7821cc1098d0fc9032ae6c700c22cf3
joshmcorreia <joshmcorreia@gmail.com>
2022-03-27 20:41:29 -0700

    Add groundwork for recognizing what each yellow letter could be

--- WordleEntry.py
+++ WordleEntry.py
@@ -7,21 +7,32 @@ class WordleEntry:
 		self.entry_matrix = None
 		self.letter_matrix = self.create_letter_matrix()
 
-	def convert_row_into_letter_list(self, row: list) -> list:
+	def get_list_of_possible_yellow_letters(self, index: int) -> list:
+		possible_letters = self.word_of_the_day_matrix.copy()
+		corresponding_letter = self.word_of_the_day_matrix[index]
+		possible_letters.remove(corresponding_letter)
+		return possible_letters
+
+	def get_possible_letter_matrix(self) -> list:
+		possible_letter_matrix = []
 		letter_list = []
-		index = 0
-		for emoji in row:
-			if emoji == "🟩":
-				corresponding_letter_of_the_day = self.word_of_the_day_matrix[index]
-				letter_list.append(corresponding_letter_of_the_day)
-			else:
-				letter_list.append(None)
-			index += 1
-		return letter_list
+		for row_index, row in enumerate(self.emoji_matrix):
+			for letter_index, emoji in enumerate(row):
+				if emoji == "🟩":
+					corresponding_letter_of_the_day = [self.word_of_the_day_matrix[letter_index]]
+					letter_list.append(corresponding_letter_of_the_day)
+				elif emoji == "🟨":
+					possible_letters = self.get_list_of_possible_yellow_letters(letter_index)
+					letter_list.append(possible_letters)
+				else:
+					letter_list.append(None) # In this instance, `None` represents that it can be any letter. This is simpler/prettier than having A-Z
+			possible_letter_matrix.append(letter_list)
+			letter_list = []
+		return possible_letter_matrix
 
 	def create_letter_matrix(self):
 		letter_matrix = []
-		for row in self.emoji_matrix:
-			letter_list = self.convert_row_into_letter_list(row)
-			letter_matrix.append(letter_list)
+		possible_letter_matrix = self.get_possible_letter_matrix()
+		for row in possible_letter_matrix:
+			print(row)
 		return letter_matrix

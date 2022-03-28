f6be57676808a950f124d25582f0f041eced69ac
joshmcorreia <joshmcorreia@gmail.com>
2022-03-27 21:03:40 -0700

    Replace None with a list of all letters

--- WordleEntry.py
+++ WordleEntry.py
@@ -25,7 +25,7 @@ class WordleEntry:
 					possible_letters = self.get_list_of_possible_yellow_letters(letter_index)
 					letter_list.append(possible_letters)
 				else:
-					letter_list.append(None) # In this instance, `None` represents that it can be any letter. This is simpler/prettier than having A-Z
+					letter_list.append(list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
 			possible_letter_matrix.append(letter_list)
 			letter_list = []
 		return possible_letter_matrix
